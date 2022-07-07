164c2354094e5990eb49691af4972cb6f5a443b6
SnailDragon <tylernielsenn@gmail.com>
2022-07-07 00:04:22 -0400

    fixed skipping

--- main.py
+++ main.py
@@ -5,7 +5,7 @@ downtimeIn = input("Enter time between tests (hh:mm:ss): ")
 downtime = int(downtimeIn[0:2]) * 60*60 + int(downtimeIn[3:5]) * 60 + int(downtimeIn[6:8])
 
 timelimitIn = input("Enter duration of test or skip (dd:hh:mm:ss): ")
-if(timelimitIn == ""):
+if(timelimitIn != ""):
     timelimit = int(timelimitIn[0:2]) * 60*60*24 + int(timelimitIn[3:5]) * 60*60 + int(timelimitIn[6:8]) * 60 + int(timelimitIn[9:11])
 else:
     timelimit = 9999999999
